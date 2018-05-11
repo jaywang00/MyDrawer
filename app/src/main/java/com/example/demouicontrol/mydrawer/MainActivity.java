50d44acf18216ff0693b452311bf492eff3bd7d1
Jay Wang <ielcw00@gmail.com>
2018-05-11 13:47:31 +0800

    V1.00_switch_Toast

--- app/src/main/java/com/example/demouicontrol/mydrawer/MainActivity.java
+++ app/src/main/java/com/example/demouicontrol/mydrawer/MainActivity.java
@@ -81,29 +81,29 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-//        switch (id){
-//            case R.id.nav_camera:
-//            case R.id.nav_gallery:
-//            case R.id.nav_slideshow:
-//            case R.id.nav_manage:
-//            case R.id.nav_share:
-//            case R.id.nav_send:
-//                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
 //        }
+
+        switch (id){
+            case R.id.nav_camera:
+            case R.id.nav_gallery:
+            case R.id.nav_slideshow:
+            case R.id.nav_manage:
+            case R.id.nav_share:
+            case R.id.nav_send:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
